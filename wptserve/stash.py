4cc56ad7b990abc1d54fc566dcf4c8ea3aaec381
James Graham <james@hoppipolla.co.uk>
2015-07-03 09:50:25 +0100

    Shared stash

--- wptserve/stash.py
+++ wptserve/stash.py
@@ -1,7 +1,20 @@
 import uuid
+from multiprocessing.managers import SyncManager, DictProxy
+
 
 #TODO: Consider expiring values after some fixed time for long-running
 #servers
+
+
+def start_server(address, authkey):
+    shared_data = {}
+
+    class DictManager(SyncManager):
+        pass
+
+    DictManager.register("get_dict", callable=lambda:shared_data, proxytype=DictProxy)
+    manager = SyncManager(("127.0.0.1", 8910), "abc")
+    manager.get_server().serve_forever()
 
 
 class Stash(object):
@@ -24,8 +37,18 @@ class Stash(object):
 
     data = {}
 
-    def __init__(self, default_path):
+    def __init__(self, address, auth, default_path):
+        self.data = self._get_proxy(address, auth)
         self.default_path = default_path
+
+    def _get_proxy(self, address, auth):
+        class DictManager(SyncManager):
+            pass
+
+        DictManager.register("get_dict")
+        manager = SyncManager(address, auth)
+        manager.connect()
+        d = manager.dict()
 
     def put(self, key, value, path=None):
         """Place a value in the stash.
@@ -36,10 +59,18 @@ class Stash(object):
                      the current request path)"""
         if path is None:
             path = self.default_path
-        if path not in self.data:
-            self.data[path] = PathStash(path)
 
-        self.data[path][key] = value
+        if value is None:
+            raise ValueError("Stash value may not be set to None")
+
+        id = (path, uuid.UUID(key))
+
+        if id in self.data:
+            raise StashError("Tried to overwrite existing stash value "
+                             "for path %s and key %s (old value was %s, new value is %s)" %
+                             (self.path, key, self.data[(path, key)], value))
+
+        self.data[id] = value
 
     def take(self, key, path=None):
         """Remove a value from the stash and return it.
@@ -50,34 +81,14 @@ class Stash(object):
         if path is None:
             path = self.default_path
 
-        if path in self.data:
-            value = self.data[path][key]
+        id = (path, uuid.UUID(key))
+
+        if id in self.data:
+            value = self.data[id]
+            del self.data[id]
         else:
             value = None
         return value
-
-
-class PathStash(dict):
-    def __init__(self, path):
-        self.path = path
-
-    def __setitem__(self, key, value):
-        key = uuid.UUID(key)
-        if value is None:
-            raise ValueError("Stash value may not be set to None")
-        if key in self:
-            raise StashError("Tried to overwrite existing stash value "
-                             "for path %s and key %s (old value was %s, new value is %s)" %
-                             (self.path, key, self[str(key)], value))
-        else:
-            dict.__setitem__(self, key, value)
-
-    def __getitem__(self, key):
-        key = uuid.UUID(key)
-        rv = dict.get(self, key, None)
-        if rv is not None:
-            del self[key]
-        return rv
 
 
 class StashError(Exception):
